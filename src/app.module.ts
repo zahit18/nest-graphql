7d3f5ca16d70a3f12fb2f0d542fc4c46af61ba39
zahit18 <zahithdez@gmail.com>
2025-04-19 21:46:16 -0700

    Apollo IDE & Some basic functions

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'; 
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
 imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      //playground: false,
+      playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloWorldModule,
   ],
--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String)
+    @Query(() => String, { description: 'hello world es lo que retorna', name: 'hellou' })
     helloWorld(): string {
         return 'hello world'
+    }
+
+    @Query(() => Float, { name: 'randomNumber' })
+    getRandomNumber(): number {
+        return Math.random() * 100
+    }
+
+    @Query(() => Int, { name: 'randomNumberFromZeroTo' })
+    getRandomNumerFromZeroTo() {
+        const max = 10
+        return Math.floor(Math.random() * max);
     }
 }
